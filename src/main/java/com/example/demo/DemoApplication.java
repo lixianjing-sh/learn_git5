91fc732088de1bf6d78be868eaa98696659b1148
15022318197 <15022318197@163.com>
2021-02-25 17:33:34 +0800

    修改，测试提交到github

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import javax.xml.ws.RequestWrapper;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("123");
         SpringApplication.run(DemoApplication.class, args);
     }
 
