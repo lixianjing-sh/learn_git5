743cb2e5b589f4eb082e0a40fa16d57537c62dc2
15022318197 <15022318197@163.com>
2021-02-25 17:37:37 +0800

    修改，测试提交到github2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("1231");
         SpringApplication.run(DemoApplication.class, args);
     }
 
